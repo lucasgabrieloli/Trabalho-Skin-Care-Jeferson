24d594e8e68ff21f88d55e13830ff6fc5a6011ae
Lucas Gabriel Oliveira de Sousa <162928019+lucasgabrieloli@users.noreply.github.com>
2024-11-02 23:04:35 -0300

    Add files via upload
    começo da página de login do site

---
+++ paglogin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="paglogin.css">
+
+</head>
+<body>
+
+    <main>
+    <h1>LOGIN</h1>
+    <form method="POST">
+    <p>NÃO TEM UMA CONTA? <a href="pagcadastro.php">CADASTRE-SE</a>!</p>
+    <label for="email">E-mail</label><br>
+    <input type="email" placeholder="Digite seu E-mail:" autofocus="true"/><br>
+    <label for="password">Senha</label><br>
+    <input type="password" placeholder="Digite sua senha:"><br>
+    <a href="">Esqueci minha senha</a><br>
+    <input type="submit" value="Acessar">
+    </form>
+</main>
+
+</body>
+</html>
+    
+</body>
+</html>
---
+++ pagprincipal.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="pagprincipal.css">
+
+</head>
+<body>
+    
+<header>
+    <h1>Brilho & <br>Glamour</h1>
+        
+    <nav>
+        <a href="" class="aheader">SOBRE NÓS</a>
+        <a href="" class="aheader">RECOMENDAÇÕES PARA SUA PELE</a>
+        <a href="" class="aheader">LOGIN</a>
+    </nav>
+
+</header>
+
+</body>
