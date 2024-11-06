b5c5ee57737d336d9c62fcc9bf1595e33f45339c
Lucas Gabriel Oliveira de Sousa <162928019+lucasgabrieloli@users.noreply.github.com>
2024-11-06 17:54:10 -0300

    Add files via upload
    terceiro commit mudanças no site BeG

--- pagsobrenos.php
+++ pagsobrenos.php
@@ -21,17 +21,27 @@
         <h2>Conheça a Brilho & Glamour!</h2>
 
         <p>Somos uma clínica de estética voltada para o bem-estar e autoestima da mulher, atuando na prestação de serviço como procedimentos no rosto e corpo e venda de produtos.
-        </p><br>
+        </p>
 
-        <p><strong>Missão:</strong> Oferecer tratamentos estéticos personalizados, com excelência, promovendo o bem-estar, a autoestima e a confiança dos nossos clientes, através de procedimentos inovadores e de alta qualidade..</p> <br>
-        <p><strong>Visão:</strong> Ser referência no setor de estética, reconhecida pela qualidade dos serviços, atendimento diferenciado e por proporcionar resultados que valorizam a beleza e saúde dos nossos clientes.</p><br>
-        <p><strong>Valores:</strong>
-        <br><strong>&emsp;Excelência:</strong> Compromisso com a qualidade e profissionalismo em todos os procedimentos.
-        <br><strong>&emsp;Inovação:</strong> Estar sempre atualizados com as melhores tecnologias e tendências do mercado estético.
-        <br><strong>&emsp;Bem-estar:</strong> Valorizar o conforto e a saúde dos nossos clientes, promovendo um ambiente relaxante e seguro.
-        <br><strong>&emsp;Ética:</strong> Respeito, transparência e integridade em todas as nossas relações.
-        <br><strong>&emsp;Satisfação do Cliente:</strong> Foco em atender e superar as expectativas de quem nos procura.
-        <br><strong>&emsp;Sustentabilidade:</strong> Buscar práticas que respeitam o meio ambiente e o uso consciente de recursos.</p>
+        <dl>
+                <dt><strong>MISSÃO:</strong></dt><br><dd>&emsp;Oferecer tratamentos estéticos personalizados, com excelência, promovendo o bem-estar, a autoestima e a confiança dos nossos clientes, através de procedimentos inovadores e de alta qualidade.. 
+                </dd><br><br>
+
+                <dt><strong>VISÃO:</strong></dt><br><dd>&emsp;Ser referência no setor de estética, reconhecida pela qualidade dos serviços, atendimento diferenciado e por proporcionar resultados que valorizam a beleza e saúde dos nossos clientes.
+                </dd><br><br>
+
+                <dt><strong>VALORES:</strong></dt><br>
+                <dd><b>Excelência:</b> Compromisso com a qualidade e profissionalismo em todos os procedimentos.</dd><br>
+                <dd><b>Inovação:</b> Estar sempre atualizados com as melhores tecnologias e tendências do mercado estético.</dd><br>
+                <dd><b>Bem-estar:</b> Valorizar o conforto e a saúde dos nossos clientes, promovendo um ambiente relaxante e seguro.</dd><br>
+                <dd><b>Ética:</b> Respeito, transparência e integridade em todas as nossas relações.</dd><br>
+                <dd><b>Satisfação do Cliente:</b> Foco em atender e superar as expectativas de quem nos procura.</dd><br>
+                <dd><b>Sustentabilidade:</b> Buscar práticas que respeitam o meio ambiente e o uso consciente de recursos.</dd><br>
+ 
+                
+
+
+        </dl>
 
 </main>
 
